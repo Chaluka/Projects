fb4ed7df08320dd88d3fc1f862e2cef352d716b9
Chaluka Salgado <chaluka.salgado@gmail.com>
2021-05-28 23:14:23 +1000

    update formattings

--- UnZipper/UnZipper.py
+++ UnZipper/UnZipper.py
@@ -18,79 +18,110 @@ __version__ = "1.0"
 
 # from tester import unZipMe
 import zipfile
-import os 
 import sys
-from os.path import isfile, join
 import glob
-from typing import TypeVar, List
-
+from datetime import date
+from typing import List
 
 class UnZipper:
-    
+
     RECURSION_DEPTH_LIMIT = 3
 
     def __init__(self) -> None:
-        self.root_dir_path = '/home/kamikaze/Documents/GitHub/Projects/UnZipper/files/' 
+        self.root_dir_path = sys.path[0] + "/"
         self.log_string = []
         self.file_extension = ".zip"
-        self.log_file_path = self.root_dir_path + 'log_file.txt'
+        self.log_file_path = self.root_dir_path + \
+            str(date.today()) + '_log.txt'
 
-    def set_directory(self, dir_path:str)->None:
+    def set_directory(self, dir_path: str) -> None:
         self.root_dir_path = dir_path
 
-    def set_file_extension(self, file_extension:str)->None:
+    def set_file_extension(self, file_extension: str) -> None:
         self.file_extension = file_extension
 
-    def __str__(self):
+    def __str__(self)->str:
         return self.cur_dir_path
-    
-    def __get_file_list(self, dir_path):
+
+    def __get_file_list(self, dir_path)->List[str]:
+        """ 
+            Returns a list of files in current directory
+
+        Args:
+            dir_path (str): path of the current directory
+
+        Returns:
+            List[str]: list of file paths
+        """
+        # generate the complete path pattern to obtain the list of files
+        # with given extension in currrent directory
+        # e.g., /Documents/GitHub/Projects/UnZipper/*.zip
         extended_path = dir_path + "*" + self.file_extension
-        print(extended_path)
+        # obtain the file list
         file_list = glob.glob(extended_path)
         return file_list
 
-    def __unzip_file(self, file, dir_path):
+    def __unzip_file(self, file, dir_path)->str:
+        """Unzip the file into the given directory
+
+        Args:
+            file (str): path of the zip file
+            dir_path (str): path of the current directory
+
+        Returns:
+            str: path of the unzipped directory
+        """
+        # extract the path of the directory that file decompressed into
         file_dir_path = file.strip(self.file_extension) + "/"
-        print("path", file_dir_path)
+        # decompress the file using zipfile module
         with zipfile.ZipFile(file, 'r') as zip_ref:
             zip_ref.extractall(dir_path)
         return file_dir_path
 
-    def extract_all_files(self, dir_path="", depth=0):
-        if depth< self.RECURSION_DEPTH_LIMIT:
+    def extract_all_files(self, dir_path="", depth=0)->None:
+        """ 
+            Extract all the compressed files found in the current directory. 
+            This is a recursive function that traverse into multi-levels of 
+            the compressed file.
+
+        Args:
+            dir_path (str, optional): path of current directory. Defaults to "".
+            depth (int, optional): recursion depth. Defaults to 0.
+        """
+        # decompress until current recursive depth does not exceeds the limit
+        if depth < self.RECURSION_DEPTH_LIMIT:
             dir_path = self.root_dir_path if not dir_path else dir_path
             file_list = self.__get_file_list(dir_path)
+            # save directory path and list of compressed files to include in log file
             self.log_string.append([dir_path, file_list])
-            if not len(file_list):
-                print(dir_path , " IS EMPTY. ABORTED..!!")
-            else:
-                print(len(file_list) ," ZIP FILES FOUND. EXTRACTING..!!")
+            # only if compressed files exist in current directory
+            if len(file_list):
                 for file in file_list:
-                    print(file, " EXTRACTED")
-                    file_dir_path = self.__unzip_file(file,dir_path)
+                    print("-->", file)
+                    file_dir_path = self.__unzip_file(file, dir_path)
+                    # recursively move to next level of compression
                     self.extract_all_files(file_dir_path, depth+1)
-    
-    def logger(self):
+
+    def __logger(self):
+        """
+            Log directory paths along with the decompressed file names
+        """
         seperator = "-"*50 + "\n"
+
         with open(self.log_file_path, 'w') as file:
             for dir_path, file_list in self.log_string:
                 file.write(dir_path+"\n")
                 for i, file_name in enumerate(file_list):
                     file_name = file_name.replace(dir_path, "")
-                    file.write('\t'+ str(i+1) +". " + file_name+"\n")
-                
+                    file.write('\t' + str(i+1) + ". " + file_name+"\n")
+
                 file.write(seperator)
-            
 
     def run(self):
         self.extract_all_files()
-        self.logger()
+        self.__logger()
 
 
 if __name__ == "__main__":
     zipper = UnZipper()
     zipper.run()
-    # g = glob.glob('/home/kamikaze/Documents/GitHub/Projects/UnZipper/files/main/f2/*.zip')
-    # print(g)
-    # extract_all_files(path,ext)
--- UnZipper/tester.py
+++ UnZipper/tester.py
@@ -8,60 +8,6 @@
     Supports both ZIP and RAR file formats.
 """
 
-__author__ = "Chaluka Salgado"
-__copyright__ = "Copyright 2020 @ Kamikaze"
-__email__ = "chaluka.salgado@gmail.com"
-__date__ = "29-Oct-2019"
-__updated__ = "28-May-2021"
-__version__ = "1.0"
-
-
-import zipfile
-from os import listdir
-from os.path import isfile, join
-import glob
-
-RECURSION_DEPTH_LIMIT = 5
-
-# def getFileList(path_directory):
-#     onlyfiles = [path_directory + f for f in listdir(path_directory) if isfile(join(path_directory, f))]
-#     return onlyfiles
-
-def getFileList(path_directory, file_extension):
-    extended_path = path_directory + "*" + file_extension
-    file_list = glob.glob(extended_path)
-    return file_list
-
-
-def unZipMe(path_to_zip_file, file_extension):
-    extract_directory_path = path_to_zip_file.strip(file_extension) + "/"
-    print("path", extract_directory_path)
-    with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
-        zip_ref.extractall(extract_directory_path)
-
-def extractAllZipFiles(path_directory, file_extension):
-    #first extact all zip files in current directory
-    file_list = getFileList(path_directory,file_extension)
-    print(file_list)
-    if len(file_list)==0:
-        print(path_directory , " IS EMPTY. ABORTED..!!")
-    else:
-        print(len(file_list) ," ZIP FILES FOUND. EXTRACTING..!!")
-        for file in file_list:
-            print(file, " EXTRACTED")
-            unZipMe(file,file_extension)
-            current_directory_path = file.strip(file_extension) + "/"
-            print(current_directory_path)
-            extractAllZipFiles(current_directory_path, file_extension)
-
-
-path_to_zip_file = "/Users/chalukasalgado/test/testfile2.zip"
-directory_to_extract = "/Users/chalukasalgado/test/extracted"
-path_directory = "/Users/chalukasalgado/test/"
-file_extension = ".zip"
-
-extractAllZipFiles(path_directory, file_extension)
-
-# extract_directory_path = path_to_zip_file.strip(file_extension)
-
-# print(extract_directory_path)
+from datetime import date
+t = date.today()
+print(str(t))
